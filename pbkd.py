c345d91dca83a2b3e8554801a392ebf1b17dc02f
pbkd <39905850+pbkd@users.noreply.github.com>
2018-06-04 15:20:56 +0300

    Update pbkd.py

--- pbkd.py
+++ pbkd.py
@@ -1,6 +1,6 @@
 import hashlib, binascii, datetime
-salt = ‘{:%H:%M:%S}’.format(datetime.datetime.now())
-password = raw_input(“Enter your password: ”)
-pbkd = hashlib.pbkdf2_hmac(“sha256”,password,salt,1)
+salt = '{:%H:%M:%S}'.format(datetime.datetime.now())
+password = raw_input("Enter your password: ")
+pbkd = hashlib.pbkdf2_hmac("sha256",password,salt,1)
 pbkd = binascii.hexlify(pbkd)
-print(“Your derived key is: ”,pbkd)
+print("Your derived key is: ",pbkd)
